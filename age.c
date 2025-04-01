375ebb840e9eb340f22525882d48d2d4e972584f
Shensi Chinolla <chinollashensi@gmail.com>
2025-04-01 14:49:38 +0000

    Class 2

---
+++ age.c
@@ -0,0 +1,15 @@
+/*This program takes age input from the user
+It stores it in the variable
+And, print the value using printf()*/
+
+#include <stdio.h>
+
+int main () {
+  
+    int age = 17; //Create integer variable
+
+    //Print the age variable
+    printf("Age: %d\n", age);
+
+    return 0;
+}
--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Print "Hello World!" to the screen 
 int main() {
 printf("Hello World!\n");
     return 0;
