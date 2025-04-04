d35b9050a2933da15ee006f9c76e3a8799f5ae0b
Shensi Chinolla <chinollashensi@gmail.com>
2025-04-04 14:34:16 +0000

    Welcome to the Snack Shop!

---
+++ variables-menu.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main () {
+
+    char snack1[] = "chocolate";
+    char snack2[] = "ruffles";
+    char snack3[] = "soda";
+    float price1 = 12.50;
+    float price2 = 20.22;
+    float price3 = 17.85;
+    int stock1 = 13;
+    int stock2 = 20;
+    int stock3 = 50;
+
+
+    printf("Welcome to the Snack Shop!\n");
+    printf("-----------------------------\n");
+    printf("1. %s - $%.2f - %d Available\n", snack1, price1, stock1);
+    printf("2. %s - $%.2f - %d Available\n", snack2, price2, stock2);
+    printf("3. %s - $%2.f - %d Available\n", snack3, price3, stock3);
+
+    return 0;
+
+}
