45306737f40858efbb4be1c3250e8997e5d07dcf
Shensi Chinolla <chinollashensi@gmail.com>
2025-04-10 16:29:21 +0000

    if-else

---
+++ if-else.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    // Excersice 1
+    int num1;
+    int num2;
+    printf("Please enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    if(num1 == num2){
+        printf("%d is equal to %d \n", num1, num2);
+    } else {
+        printf("%d is NOT equal to %d \n", num1, num2);
+    }
+   
+    //Excersice 2
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    int 
+    if (number  ){
+        printf("%d is an even number", number);
+    } else {
+        printf("%d is not an even number", number);
+    }
+
+    return 0;
+}
