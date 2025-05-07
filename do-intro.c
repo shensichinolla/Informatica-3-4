b72b387520b2b52ce174309d9854a1a9ad367774
Shensi Chinolla <chinollashensi@gmail.com>
2025-05-07 16:15:08 +0000

    do-intro

--- do-intro.c
+++ do-intro.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main(){
+    int n;
+    int count = 0;
 
-    int number;
+    printf("Enter a positive number: ");
+    scanf(" %d ", &n);
 
-    printf("Enter a number: ");
-    scanf("%d", &number);
+    do {
+        n = n / 10;
+        // n /= 10
 
-    
+        ++count;
+    } while (n != 0);
 
-
-
-return 0;
+    printf("Number of digits: %d\n", count);
 }
