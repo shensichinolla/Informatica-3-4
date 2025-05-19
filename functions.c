d2ca513252cbd8235d396d332c3a80941fa9766a
Shensi Chinolla <chinollashensi@gmail.com>
2025-05-19 16:25:58 +0000

    functions.c

--- functions.c
+++ functions.c
@@ -14,10 +14,33 @@ int main(){
     int user_age;
     printf("Enter your age: ");
     scanf("%d", &user_age);
+
+    int user_selection;
+    printf("Select a planet:\n1. Mercury\n2. Venus\n3. Earth\n4. Mars\n5. Jupiter\n6. Saturn\n7. Uranus\n8. Neptune\nType your choice: ");
+    scanf("%d", &user_selection);
+
+    if(user_selection == 1){
+        calculateAge(user_age, MERCURY);
+    }else if(user_selection == 2){
+        calculateAge(user_age, VENUS);
+    }else if(user_selection == 3){
+        calculateAge(user_age, EARTH);
+    }else if(user_selection == 4){
+        calculateAge(user_age, MARS);
+    }else if(user_selection == 5){
+        calculateAge(user_age, JUPITER);
+    }else if(user_selection == 6){
+        calculateAge(user_age, SATURN);
+    }else if(user_selection == 7){
+        calculateAge(user_age, URANUS);
+    }else if(user_selection == 8){
+        calculateAge(user_age, NEPTUNE);
+    }
+
 }
 
 float calculateAge(int age, float orbit){
         float result;
         result = age / orbit;
-        return result;
+        printf("You would be %.2f years old\n", result);
     }
